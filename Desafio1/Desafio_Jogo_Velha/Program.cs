2bd263b3351709722580f9df3020ae249043641a
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-18 20:47:40 -0300

    criado sistema de jogadas e novo jogo

--- Desafio1/Desafio_Jogo_Velha/Program.cs
+++ Desafio1/Desafio_Jogo_Velha/Program.cs
@@ -7,28 +7,91 @@ namespace Desafio_Jogo_Velha
     {
         static void Main(string[] args)
         {
-            String[,] jogo = new string[3, 3];
-            String jogador = " X ";
-            IniciaTabuleiro(jogo);
-            ApresentaTabuleiro(jogo);
+            bool controle = true;
+            do { 
+                
+                String[,] jogo = new string[4, 4];
+                String jogador = " X ";
+                
+                
+                Console.WriteLine("Bem vindo ao Jogo Da velha da Atos!");
+                Console.WriteLine("Escolha posições numa matriz 3X3 para colocar X ou O");
+                Console.WriteLine("Digite uma opção válida:\n1 - Novo Jogo\n2 - sair");
+                char opcao = char.Parse(Console.ReadLine());
+                switch(opcao){
+
+                    case '1':
+                        Console.Clear();
+                        IniciaTabuleiro(jogo);
+                        ApresentaTabuleiro(jogo);
+                        bool controleVitoria = true;
+                        do
+                        {
+
+                            Console.WriteLine($"Diga qual posição da matriz deseja preencher com {jogador}");
+                            int linha = int.Parse(Console.ReadLine());
+                            int coluna = int.Parse(Console.ReadLine());
+                            Console.Clear();
+                            if (linha > 0 && linha <= 3)
+                            {
+                                if (coluna > 0 && coluna <= 3)
+                                {
+
+                                    if (VerificaEspacoVazio(jogo, linha, coluna))
+                                    {
+                                        Jogar(jogo, jogador, linha, coluna);
+                                        controleVitoria = !VerificaVitoria(jogo, jogador);
+                                        jogador = TrocaJogador(jogador);
+                                    }
+                                    else ApresentaTabuleiro(jogo);
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Posição inexistente");
+                                    ApresentaTabuleiro(jogo);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Posição inexistente");
+                                ApresentaTabuleiro(jogo);
+                            }
+                            
+                        } while (controleVitoria);
+                        break;
+                    case '2':
+                        controle = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+
+                }
+
+                
+            }while (controle);
 
         }
 
         static void IniciaTabuleiro(String[,] jogo)
         {
-            for (int i = 0; i < 3; i++)
+            jogo[0, 0] = " ";
+            for (int i = 1; i <= 3; i++)
             {
-                for (int j = 0; j < 3; j++)
+                jogo[i, 0] = "" + i;
+                for (int j = 1; j <= 3; j++)
                 {
                     jogo[i, j] = " - ";
+                    jogo[0, j] = $" {j} ";
                 }
             }
         }
         static void ApresentaTabuleiro(String[,] jogo)
         {
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i <= 3; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j <= 3; j++)
                 {
                     Console.Write(jogo[i, j]);
                 }
@@ -41,10 +104,10 @@ namespace Desafio_Jogo_Velha
             int pontos = 0;
             bool vitoria = false;
             //linhas
-            for (int i = 0; i < 3; i++)
+            for (int i = 1; i <= 3; i++)
             {
                 pontos = 0;
-                for (int c = 0; c < 3; c++)
+                for (int c = 1; c <= 3; c++)
                 {
                     if (jogo[i,c] == jogador)
                     {
@@ -58,10 +121,10 @@ namespace Desafio_Jogo_Velha
                 }
             }
             //colunas
-            for (int c = 0; c < 3; c++)
+            for (int c = 1; c <= 3; c++)
             {
                 pontos = 0;
-                for (int i = 0; i < 3; i++)
+                for (int i = 1; i <= 3; i++)
                 {
                     if (jogo[i,c] == jogador)
                     {
@@ -76,7 +139,7 @@ namespace Desafio_Jogo_Velha
             }
             //diagonais
             pontos = 0;
-            for (int d = 0; d < 3; d++)
+            for (int d = 1; d <= 3; d++)
             {
 
                 if (jogo[d,d] == jogador)
@@ -92,8 +155,8 @@ namespace Desafio_Jogo_Velha
 
             }
             pontos = 0;
-            int l = 0;
-            for (int c = 2; c >= 0; c--)
+            int l = 1;
+            for (int c = 3; c >= 1; c--)
             {
                 if (jogo[l,c] == jogador)
                 {
@@ -113,10 +176,10 @@ namespace Desafio_Jogo_Velha
             else
             {
                 int testa = 0;
-                for (int c = 0; c < 3; c++)
+                for (int c = 1; c <= 3; c++)
                 {
 
-                    for (int i = 0; i < 3; i++)
+                    for (int i = 1; i <= 3; i++)
                     {
                         if (jogo[i,c] != " - ")
                         {
@@ -127,8 +190,8 @@ namespace Desafio_Jogo_Velha
                 if (testa == 9)
                 {
                    Console.WriteLine("Deu empate");
-                    return false;
-                    //NovoJogo();
+                    return true;
+                    
 
                 }
                 else
@@ -138,9 +201,9 @@ namespace Desafio_Jogo_Velha
             }
 
         }
-        static void TrocaJogador(String jogador)
+        static String TrocaJogador(String jogador)
         {
-            jogador = (jogador == " X ") ? " O " : " X ";
+            return  (jogador == " X ") ? " O " : " X ";
         }
         static bool VerificaEspacoVazio (String[,] jogo, int linha, int coluna) 
         {
@@ -153,20 +216,24 @@ namespace Desafio_Jogo_Velha
             {
                 
                 jogo[linha,coluna] = jogador;
-                TrocaJogador(jogador);
+                 
+                ApresentaTabuleiro(jogo);
                 if (VerificaVitoria(jogo, jogador))
                 {
                     TrocaJogador(jogador);
-                    Console.WriteLine($"Jogador {jogador} venceu!");
-          
+                    Console.WriteLine($"Jogador {jogador} venceu!\n\n");
+                     
                 }                
 
             }
             else
             {
                 Console.WriteLine("Espaço já definido!");
+                ApresentaTabuleiro(jogo);
             }
         }
+
+        
            
     }
 }
