ed7017d319ed4d7d7ee917ad825625821cda0419
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-18 15:45:21 -0300

    criando projeto jogo da velha, desenvolvida funcionalidade de renderizar tabuleiro

---
+++ Desafio1/Desafio_Jogo_Velha/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Desafio_Jogo_Velha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[,] jogo = new string[3,3];
+            IniciaTabuleiro(jogo);
+            ApresentaTabuleiro(jogo);
+
+        }
+
+        static void IniciaTabuleiro(String[,] jogo)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    jogo[i, j] = "-";
+                }
+            }
+        }
+        static void ApresentaTabuleiro(String[,] jogo)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(jogo[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
